9d8154774f07164fe5ee8be6ffc1021637d4f500
Eli Klein <eliklein02@gmail.com>
2025-05-06 22:36:35 -0400

    db yml changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,9 +6,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
-  # no cable
-  config.action_cable.mount_path = nil
-
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
