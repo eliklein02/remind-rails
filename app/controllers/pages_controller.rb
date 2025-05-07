dda00e9e6ec1237f643961a5fb98e50991af91a0
Eli Klein <eliklein02@gmail.com>
2025-05-07 15:25:37 -0400

    p policy and tos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_organization!, only: [ :privacy_policy, :terms_of_service, :about ]
+
   def home
     @contacts = Contact.all.sort_by { |c| c.name.to_s }
   end
