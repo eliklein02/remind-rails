9ce823234cc1a448297565337615ee95c8a99685
Eli Klein <eliklein02@gmail.com>
2025-05-15 00:44:54 -0400

    conditional navbar and footer if not app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   set_current_tenant_through_filter
   before_action :authenticate_organization!
   before_action :set_organization_as_current_tenant
+  helper_method :is_app?
   # allow_browser versions: :modern
 
 
   def set_organization_as_current_tenant
     set_current_tenant current_organization
+  end
+
+  def is_app?
+    request.user_agent == "react-native"
   end
 end
