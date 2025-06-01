9ddb0485abca641fecee988444e4fad34c842357
Eli Klein <72769850+eliklein02@users.noreply.github.com>
2025-06-01 19:00:01 -0400

    opted in update

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,17 @@
 class ApiController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [ :textgrid_webhook ]
   def textgrid_webhook
-    puts params
+    contacts = Contact.where(phone: params["From"])
+    message = params["Body"]
+    case message
+    when "opt out", "optout", "stop", "unsubscribe", "exit", "cancel", "#exit", "#stop"
+      if contact
+        contacts.each do |c|
+          c.update(opted_in_status: 2)
+        end
+      else
+        nil
+      end
+    end
   end
 end
